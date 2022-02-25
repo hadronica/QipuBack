9a85cbec6e3c5e2e0ad70c740a746fe505f228e8
Alonso Cortez <mcorteztouzett@gmail.com>
2022-02-25 12:56:35 -0500

    auth added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,14 +5,27 @@ const User=require('../../models/usuarios')
 
 const router=require('express').Router()
 
-router.post('/',[
+router.post('/ruc',[
     check('ruc','El ruc debe ser de 11 digitos').isLength({min:11,max:11}),
     validarCampo
 ],async(req,res)=>{
     const ruc=req.body.ruc
     const existeUser=await User.findOne({where:{ruc:ruc}})
     if(existeUser){
-        res.status(401).json({validate:false,msg:`El ruc ${ruc} ya existe`})
+        return res.status(401).json({validate:false,msg:`El ruc ${ruc} ya existe`})
+    }else{
+        return res.status(200).json({validate:true})
+    }
+ 
+})
+router.post('/email',[
+    check('email','El email es invalido').isEmail(),
+    validarCampo
+],async(req,res)=>{
+    const email=req.body.email
+    const existeUser=await User.findOne({where:{email:email}})
+    if(existeUser){
+        return res.status(401).json({validate:false,msg:`El email ${email} ya existe`})
     }else{
         return res.status(200).json({validate:true})
     }
